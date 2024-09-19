6cb374f36ed4cecc38ad9b1d0aefdf2d60af2ba9
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:01:54 -0300

    utils para hash de senha e password policy

---
+++ Ecommerce.Backend/Ecommerce.Dominio/Util/PasswordHash.cs
@@ -0,0 +1,15 @@
+﻿namespace Ecommerce.Dominio.Util
+{
+    public static class PasswordHash
+    {
+        public static string HashPassword(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+
+        public static bool VerificarSenha(string senhaFornecida, string hashArmazenado)
+        {
+            return BCrypt.Net.BCrypt.Verify(senhaFornecida, hashArmazenado);
+        }
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.Dominio/Util/ValidarSenha.cs
@@ -0,0 +1,19 @@
+﻿namespace Ecommerce.Dominio.Util
+{
+    public static class PasswordPolicy
+    {
+        public static bool ValidarSenha(string senha)
+        {
+            if (senha.Length < 8 ||
+                !senha.Any(char.IsUpper) ||
+                !senha.Any(char.IsLower) ||
+                !senha.Any(char.IsDigit) ||
+                !senha.Any(c => "!@#$%^&*()".Contains(c)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
