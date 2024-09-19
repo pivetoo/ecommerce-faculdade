ff2fad9bcb2e3a5eb0bb85b0ea2c3d70d92a01b3
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:13:21 -0300

    implementação de login token jwt e testando o [Authorize]

---
+++ Ecommerce.Backend/Ecommerce.API/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.API.DTOs;
+using Ecommerce.Dominio.Services;
+using Ecommerce.Dominio.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly UsuarioService _usuarioService;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(UsuarioService usuarioService, IConfiguration configuration)
+        {
+            _usuarioService = usuarioService;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO loginDto)
+        {
+            var usuario = await _usuarioService.ConsultaUsuarioPorEmail(loginDto.Email);
+            if (usuario == null)
+            {
+                return Unauthorized("Usuário ou senha inválidos.");
+            }
+
+            var token = JwtToken.GerarToken(usuario, _configuration);
+            var isAdmin = usuario.IsAdmin;
+
+            return Ok(new { token, isAdmin });
+        }
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.API/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.API.DTOs
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
