d10f2ba2b4bd535a333c41b658afd7a4450a9285
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:02:59 -0300

    email tools, implementar futuramente envio de email corretamente

---
+++ Ecommerce.Backend/Ecommerce.Dominio/Util/EmailTools.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Ecommerce.Dominio.Util
+{
+    public static class EmailTools
+    {
+        public static void EnviarEmailResetSenha(string email, string linkDeReset)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("rpiveto3@gmail.com", "wnbe sfuk pyaz yzak"),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("rpiveto3@gmail.com"),
+                Subject = "Recuperação de Senha",
+                Body = $"Clique no link para resetar sua senha: {linkDeReset}",
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(email);
+
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
