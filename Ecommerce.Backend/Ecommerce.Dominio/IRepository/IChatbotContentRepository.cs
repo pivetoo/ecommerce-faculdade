89e1255602fb76ab0c1fa0d7366f2950772c338f
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:09:01 -0300

    implementação para armazenar o conteudo de aprendizado do chatbot no banco de dados

---
+++ Ecommerce.Backend/Ecommerce.Dominio/IRepository/IChatbotContentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Dominio.Entities;
+
+namespace Ecommerce.Dominio.IRepository
+{
+    public interface IChatbotContentRepository
+    {
+        Task<ChatbotContent> Retornar(long id);
+        Task<IEnumerable<ChatbotContent>> RetornarTodos();
+        Task Incluir(ChatbotContent chatBot);
+        Task Atualizar(ChatbotContent chatBot);
+        Task Excluir(ChatbotContent chatBot);
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.Repositorio/Repositories/ChatbotContentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Dominio.Entities;
+using Ecommerce.Dominio.IRepository;
+using Ecommerce.Repositorio.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.Repositories
+{
+    public class ChatbotContentRepository : IChatbotContentRepository
+    {
+        private readonly EcommerceDbContext _context;
+
+        public ChatbotContentRepository(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ChatbotContent> Retornar(long id)
+        {
+            return await _context.ChatbotContents.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<IEnumerable<ChatbotContent>> RetornarTodos()
+        {
+            return await _context.ChatbotContents.ToListAsync();
+        }
+
+        public async Task Incluir(ChatbotContent chatBot)
+        {
+            await _context.ChatbotContents.AddAsync(chatBot);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Atualizar(ChatbotContent chatBot)
+        {
+            _context.ChatbotContents.Update(chatBot);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Excluir(ChatbotContent chatBot)
+        {
+            _context.ChatbotContents.Remove(chatBot);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
