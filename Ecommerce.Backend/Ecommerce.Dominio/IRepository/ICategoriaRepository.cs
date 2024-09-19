957480f258c6d41bddafa5492c67bca395b637c7
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 16:56:27 -0300

    adicionado services e repositories necessario modificação futuramente

---
+++ Ecommerce.Backend/Ecommerce.Dominio/IRepository/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Dominio.Entities;
+
+namespace Ecommerce.Dominio.IRepository
+{
+    public interface ICategoriaRepository
+    {
+        Task<Categoria> Retornar(long id);
+        Task<IEnumerable<Categoria>> RetornarTodos();
+        Task Incluir(Categoria categoria);
+        Task Atualizar(Categoria categoria);
+        Task Excluir(Categoria categoria);
+    }
+}
