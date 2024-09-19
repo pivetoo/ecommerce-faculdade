f38fb7b7d7667658a01c51fa3d5b17fb5d02f744
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:10:13 -0300

    adicionado controller de avaliacao

---
+++ Ecommerce.Backend/Ecommerce.API/Controllers/AvaliacaoController.cs
@@ -0,0 +1,53 @@
+﻿using Ecommerce.API.DTOs;
+using Ecommerce.Dominio.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvaliacaoController : ControllerBase
+    {
+        private readonly AvaliacaoService _avaliacaoService;
+
+        public AvaliacaoController(AvaliacaoService avaliacaoService)
+        {
+            _avaliacaoService = avaliacaoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarTodasAvaliacoes()
+        {
+            var avaliacoes = await _avaliacaoService.ListarTodasAvaliacoes();
+            return Ok(avaliacoes);
+        }
+
+        [HttpGet("produto/{produtoId}")]
+        public async Task<IActionResult> ListarAvaliacoesPorProduto(long produtoId)
+        {
+            var avaliacoes = await _avaliacaoService.ListarAvaliacoesPorProduto(produtoId);
+            return Ok(avaliacoes);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionarAvaliacao(AvaliacaoDTO avaliacaoDto)
+        {
+            await _avaliacaoService.AdicionarAvaliacao(avaliacaoDto.UsuarioId, avaliacaoDto.ProdutoId, avaliacaoDto.Nota, avaliacaoDto.Comentario);
+            return Ok("Avaliação adicionada com sucesso!");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarAvaliacao(long id, AvaliacaoDTO avaliacaoDto)
+        {
+            await _avaliacaoService.AtualizarAvaliacao(id, avaliacaoDto.Nota, avaliacaoDto.Comentario);
+            return Ok("Avaliação atualizada com sucesso!");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> ExcluirAvaliacao(long id)
+        {
+            await _avaliacaoService.ExcluirAvaliacao(id);
+            return Ok("Avaliação excluída com sucesso!");
+        }
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.API/DTOs/AvaliacaoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.API.DTOs
+{
+    public class AvaliacaoDTO
+    {
+        public long UsuarioId { get; set; }
+        public long ProdutoId { get; set; }
+        public int Nota { get; set; }
+        public string Comentario { get; set; }
+    }
+}
