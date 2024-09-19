873ff09a977627118aed10b833c83c8417362e2c
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:15:05 -0300

    middleware de exception, nao esta do jeito certo, precisa de modificações futuramente

---
+++ Ecommerce.Backend/Ecommerce.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.API.Middlewares.Helpers;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+
+namespace Ecommerce.API.Middlewares
+{
+    public class ErrorHandlingMiddleware : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            switch (exception)
+            {
+                case AppException e:
+                    httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    break;
+                case KeyNotFoundException e:
+                    httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                    break;
+                default:
+                    httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    break;
+            }
+
+            var result = JsonSerializer.Serialize(new { message = exception?.Message });
+            await httpContext.Response.WriteAsync(result, cancellationToken);
+
+            return true;
+        }
+    }
+}
