82d58bd68a00f0b5f96db948d5b77b642ce28dbe
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 17:18:21 -0300

    implementado chatbot controller, para pergunta ao chatbot

---
+++ Ecommerce.Backend/Ecommerce.API/Controllers/ChatbotController.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.API.DTOs;
+using Ecommerce.Dominio.Util;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatbotController : ControllerBase
+    {
+        private readonly ChatbotOpenAI _chatBot;
+
+        public ChatbotController(ChatbotOpenAI chatBot)
+        {
+            _chatBot = chatBot;
+        }
+
+        [HttpPost("pergunta")]
+        public async Task<IActionResult> FazerPergunta(PerguntaDTO perguntaDto)
+        {
+            try
+            {
+                var resposta = await _chatBot.ObterRespostaAsync(perguntaDto.Pergunta);
+                return Ok(new { Resposta = resposta });
+            }
+            catch (HttpRequestException ex)
+            {
+                return StatusCode(500, $"Erro ao acessar o ChatBot: {ex.Message}");
+            }
+        }
+    }
+}
