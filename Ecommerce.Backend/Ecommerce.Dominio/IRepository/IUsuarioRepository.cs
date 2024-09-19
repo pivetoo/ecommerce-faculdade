03db92f305ac4c3ee2e312374d8d613b7f3038c2
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-19 16:52:30 -0300

    camada de repositorio usuario

---
+++ Ecommerce.Backend/Ecommerce.Dominio/IRepository/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Dominio.Entities;
+
+namespace Ecommerce.Dominio.IRepository
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> Retornar(long id);
+        Task<Usuario> ConsultaPorEmail(string email);
+        Task Incluir(Usuario usuario);
+        Task Atualizar(Usuario usuario);
+        Task Excluir(Usuario usuario);
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.Repositorio/Repositories/UsuarioRepository.cs
@@ -0,0 +1,46 @@
+﻿using Ecommerce.Dominio.Entities;
+using Ecommerce.Dominio.IRepository;
+using Ecommerce.Repositorio.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly EcommerceDbContext _context;
+
+        public UsuarioRepository(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Usuario> Retornar(long id)
+        {
+            return await _context.Usuarios.FindAsync(id);
+        }
+
+        public async Task<Usuario> ConsultaPorEmail(string email)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(e => e.Email == email);
+        }
+
+        public async Task Incluir(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Atualizar(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Excluir(Usuario usuario)
+        {
+            await _context.Usuarios.FindAsync(usuario.Id);
+            _context.Usuarios.Remove(usuario!);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
