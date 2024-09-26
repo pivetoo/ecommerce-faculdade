4bc08801d43b84d8a61fb9cb72e19ba4ad7eaf66
PedroNalom <pedronalon2005@gmail.com>
2024-09-25 21:49:28 -0300

    Criação_Tela_Login_Criar_Conta_Recuperar_Senha

--- Ecommerce.Frontend/src/App.vue
+++ Ecommerce.Frontend/src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { useRouter, RouterView } from 'vue-router';
 
 </script>
 
 <template>
+    <<router-link to="/login">Login teste</router-link>>
+    <<router-link to="/recuperarSenha">Recuperar teste</router-link>>
+    <<router-link to="/"> Home teste</router-link>>
+    <<router-link to="/criarConta"> CriarConta teste</router-link>>
+    <RouterView />
 
 </template>
 
---
+++ Ecommerce.Frontend/src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>TESTE</h1>
+</template>
