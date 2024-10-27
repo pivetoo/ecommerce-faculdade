ea926b1c0e7cb1fa77b22eca736ee7a2881ff47e
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-10-27 00:33:05 -0300

    service produtorelacao

---
+++ Ecommerce.Backend/Ecommerce.Application/Services/ProdutoRelacaoService.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Dominio.Entities;
+using Ecommerce.Dominio.IRepository;
+using Ecommerce.Dominio.ValueObjects;
+
+namespace Ecommerce.Application.Services
+{
+    public class ProdutoRelacaoService
+    {
+        private readonly IProdutoRelacaoRepository _produtoRelacaoRepository;
+
+        public ProdutoRelacaoService(IProdutoRelacaoRepository produtoRelacaoRepository)
+        {
+            _produtoRelacaoRepository = produtoRelacaoRepository;
+        }
+
+        public async Task<List<Produto>> ObterProdutosEquivalentes(long produtoId)
+        {
+            return await _produtoRelacaoRepository.ObterProdutosRelacionados(produtoId, TipoRelacao.Equivalente);
+        }
+
+        public async Task<List<Produto>> ObterProdutosComplementares(long produtoId)
+        {
+            return await _produtoRelacaoRepository.ObterProdutosRelacionados(produtoId, TipoRelacao.Complementar);
+        }
+    }
+}
---
+++ Ecommerce.Backend/Ecommerce.Dominio/IService/IProdutoRelacaoService.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.Dominio.Entities;
+
+namespace Ecommerce.Dominio.IService
+{
+    public interface IProdutoRelacaoService
+    {
+        Task<List<Produto>> ObterProdutosEquivalentes(long produtoId);
+        Task<List<Produto>> ObterProdutosComplementares(long produtoId);
+    }
+}
