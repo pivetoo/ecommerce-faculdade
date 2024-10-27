7476157a8fb612a7f3bd9b2296515ef17c775529
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-10-27 00:31:58 -0300

    entidade da relacao dos produtos

---
+++ Ecommerce.Backend/Ecommerce.Repositorio/Repositories/ProdutoRelacaoRepository.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Dominio.Entities;
+using Ecommerce.Dominio.IRepository;
+using Ecommerce.Dominio.ValueObjects;
+using Ecommerce.Repositorio.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repositorio.Repositories
+{
+    public class ProdutoRelacaoRepository : IProdutoRelacaoRepository
+    {
+        private readonly EcommerceDbContext _context;
+
+        public ProdutoRelacaoRepository(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Produto>> ObterProdutosRelacionados(long produtoId, TipoRelacao tipoRelacao)
+        {
+            return await _context.ProdutoRelacoes
+                .Where(pr => pr.ProdutoId == produtoId && pr.Tipo == tipoRelacao)
+                .Select(pr => pr.ProdutoRelacionado)
+                .ToListAsync();
+        }
+    }
+}
